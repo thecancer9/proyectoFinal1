f09c56684760f41ea72e19cba08a8a92f34cabb6
thecancer9 <thecancer9@gmail.com>
2022-10-12 20:27:00 -0400

    arreglando

--- frontend/proyectoFinalFrontend/src/app/app-routing.module.ts
+++ frontend/proyectoFinalFrontend/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    loadChildren: () => import('./landing/landing.module').then(mod => mod.LandingModule)
+  },
+  {
+    path: '**',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
