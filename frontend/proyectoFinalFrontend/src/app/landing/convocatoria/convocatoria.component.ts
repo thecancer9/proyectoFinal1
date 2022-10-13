77ff5d5f29866290f35fb717d7414b6be6f08980
thecancer9 <thecancer9@gmail.com>
2022-10-12 20:29:50 -0400

    crrre

---
+++ frontend/proyectoFinalFrontend/src/app/landing/convocatoria/convocatoria.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-convocatoria',
+  templateUrl: './convocatoria.component.html',
+  styleUrls: ['./convocatoria.component.scss']
+})
+export class ConvocatoriaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
