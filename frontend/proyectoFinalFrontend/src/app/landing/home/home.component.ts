97d914e33d8758fab23ed547be2ce1ead9009d0d
thecancer9 <thecancer9@gmail.com>
2022-10-15 00:21:55 -0400

    validadores

--- frontend/proyectoFinalFrontend/src/app/landing/home/home.component.ts
+++ frontend/proyectoFinalFrontend/src/app/landing/home/home.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { emailValidator } from './email-validator.directive';
 
 interface IUser {
-  unserName: string
+  userLogin: string;
+  userRegister: string;
+  passwordLogin: string;
+  showPassword: boolean;
+  passwordRegister: string;
+  emailRegister: string;
 }
 
 @Component({
@@ -13,13 +19,15 @@ interface IUser {
 })
 export class HomeComponent implements OnInit {
 
-  registerUser!: FormGroup;
+  loginForm!: FormGroup;
+  registerForm!: FormGroup;
   user: IUser
+  loginSubmitted = false;
+  passwordSubmitted = false;
+  userRegisterSubmitted=false;
+  passwordRegisterSubmitted=false;  
+  emailSubmitted=false;
 
-  loginForm: FormGroup = new FormGroup({
-    loginUser: new FormControl(''),
-    loginPassword: new FormControl('')
-  })
   submitted = false;
 
   constructor(
@@ -30,49 +38,118 @@ export class HomeComponent implements OnInit {
 
 
   ngOnInit(): void {
-    //register
-    this.registerUser = new FormGroup({
-       userName: new FormControl('',[
-         Validators.required,
-         Validators.minLength(1),
-         Validators.maxLength(5)
-       ])
-     });
-
     // login
-    this.loginForm = this.fb.group({
-      loginUser: ['', Validators.required],
-      loginPassword: ['',Validators.required]
+    this.loginForm = new FormGroup({
+      userLogin: new FormControl(this.user.userLogin, [
+        Validators.required
+      ]),
+      passwordLogin: new FormControl(this.user.passwordLogin, [
+        Validators.required
+      ]),
+    })
+
+    //register
+    this.registerForm = new FormGroup({
+      userRegister: new FormControl(this.user.userRegister, [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(20)
+      ]),
+      passwordRegister: new FormControl(this.user.passwordRegister, [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(20)
+      ]),
+      emailRegister: new FormControl(this.user.emailRegister, [
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(50),
+        emailValidator()
+      ]),
+
     });
   }
 
-  get f(): {[key: string]:AbstractControl} {
-    return this.loginForm.controls;
+  get userLogin() {
+    return this.loginForm.get('userLogin')!;
+  }
+  get passwordLogin() {
+    return this.loginForm.get('passwordLogin')!;
+  }
+  get userRegister() {
+    return this.registerForm.get('userRegister')!;
+  }
+  get passwordRegister() {
+    return this.registerForm.get('passwordRegister')!;
+  }
+  get emailRegister() {
+    return this.registerForm.get('emailRegister')!;
   }
 
-  get userName() {
-    return this.registerUser.get('userName')!;
-  }
-
-  /**
-   * Validate
- : void  */
   public Validate(): void {
-    if (this.registerUser.invalid) {
+    if (this.userLogin.invalid) {
+      for (const control of Object.keys(this.loginForm.controls)) {
+        this.loginForm.controls[control].markAsTouched();
+      }
       return;
     }
 
-    this.user = this.registerUser.value
+    this.user = this.loginForm.value
+    console.info('Nombre:', this.user.userLogin);
+    console.info('Contraseña:', this.user.passwordLogin);
+    console.info('Usuario:', this.user.userRegister);
+    console.info('Contraseña:', this.user.passwordRegister);
+    console.info('Correo:', this.user.emailRegister);
   }
-
-  onSubmit(): void {
-    this.submitted = true;
+  
+  loginSubmit(): void {
+    this.loginSubmitted = true;
 
     
     if (this.loginForm.valid) {
       this.router.navigateByUrl('')
       // return;
     }
+  }
+  passwordSubmit(): void {
+    this.passwordSubmitted = true;
+
+    
+    if (this.loginForm.valid) {
+      this.router.navigateByUrl('')
+      // return;
+    }
+  }
+  userRegisterSubmit(): void {
+    this.userRegisterSubmitted = true;
+
+    
+    if (this.registerForm.valid) {
+      this.router.navigateByUrl('')
+      // return;
+    }
+  }
+  passwordRegisterSubmit(): void {
+    this.passwordRegisterSubmitted = true;
+
+    
+    if (this.registerForm.valid) {
+      this.router.navigateByUrl('')
+      // return;
+    }
+  }
+  emailSubmit(): void {
+    this.emailSubmitted = true;
+
+    
+    if (this.registerForm.valid) {
+      this.router.navigateByUrl('')
+      // return;
+    }
+  }
+  onReset(): void {
+    this.submitted = false;
+    this.registerForm.reset();
   }
 
 }
